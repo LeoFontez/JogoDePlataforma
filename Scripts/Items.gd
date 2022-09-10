faf12c931851a84788f37813bcb926f2b761015e
Leo Fontes <79946719+LeoFontez@users.noreply.github.com>
2022-09-10 11:30:49 -0300

    HUD V.1
    Adição de HUD na tela inicial
    Adição de contador de frutas coletadas no HUD
    Adição de script Global

--- Scripts/Items.gd
+++ Scripts/Items.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
+export var fruits = 1
+
 func _on_Items_body_entered(body):
 	$anim.play("Collected")
-
+	Global.fruits += fruits
+	print(Global.fruits)
+	
 func _on_anim_animation_finished(anim_name):
 	if anim_name == "Collected":
 		queue_free()
